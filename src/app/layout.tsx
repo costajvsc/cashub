ebceb909bde8ffbf500e76a6e84e6a2c6413a3f4
Victor Costa <costa.jvsc@gmail.com>
2025-04-26 23:12:54 -0300

    feat: add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -41,6 +42,7 @@ export default function RootLayout({
                     </AuthProvider>
                     <Toaster richColors />
                 </ThemeProvider>
+                <Analytics />
             </body>
         </html>
     );
